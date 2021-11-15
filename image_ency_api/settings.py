2c01e1da417bdabe23191979ef54b409627bbf92
karthik0309 <karthik392001@gmail.com>
2021-11-15 22:10:29 +0530

    first commit

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -167,7 +167,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 
 # Default primary key field type
