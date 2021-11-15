3ad4a2755a6edd3dfd28c10d8adf5954c964adb5
karthik0309 <karthik392001@gmail.com>
2021-11-15 21:45:45 +0530

    Updated

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -167,6 +167,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
