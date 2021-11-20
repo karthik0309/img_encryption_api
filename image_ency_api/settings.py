e37b71376d854edcfa610e57780bfd6554111005
karthik0309 <karthik392001@gmail.com>
2021-11-20 19:28:41 +0530

    Fixed cloudinary issues

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -177,6 +177,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
