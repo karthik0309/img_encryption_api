442856f9cb7f9ee2c1fd400b3ca9e90b57b0c83b
karthik0309 <karthik392001@gmail.com>
2021-11-20 21:17:02 +0530

    new heroku

--- image_ency_api/urls.py
+++ image_ency_api/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/',LoginViewset.as_view())
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
