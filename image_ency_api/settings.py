92e0b556d3f3a64901466be8da39d01d2dedcee7
karthik0309 <karthik392001@gmail.com>
2021-11-20 19:41:20 +0530

    Fixed issues

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -177,9 +177,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
