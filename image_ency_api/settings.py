4cdb9a4894c40e7e134364bf18dc7e1252cf7002
karthik0309 <karthik392001@gmail.com>
2021-11-20 20:11:20 +0530

    db errors fixed

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -173,14 +173,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
