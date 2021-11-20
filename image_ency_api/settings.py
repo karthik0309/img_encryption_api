ff5f479619a5ad6f32921f3be1ee991809123586
karthik0309 <karthik392001@gmail.com>
2021-11-20 20:53:23 +0530

    db errors fixed

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-5ue3!!#e_ys96pf4r@mf3=n1qwvvdaa!@@&o8i4pb$y*ax9g5$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
