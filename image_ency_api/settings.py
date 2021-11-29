4f7d84eb67f8f77c5146c3be16f013f529ec80c2
Karthik Belida <Karthik392001@gmail.com>
2021-11-29 19:55:26 +0530

    shifted DB

--- image_ency_api/settings.py
+++ image_ency_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import cloudinary
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 
@@ -121,13 +122,16 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES={
+    'default':dj_database_url.config()
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
